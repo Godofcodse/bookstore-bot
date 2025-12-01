326f7e47e0d4de47e254d8e5ef215a6e2feb16b6
Godofcodse <aminjafari2007@gmail.com>
2025-12-01 19:24:17 +0330

    Update init file

--- database/__init__.py
+++ database/__init__.py
@@ -2,6 +2,7 @@ from .connection import get_db_connection
 from .DDL import create_tables
 from .DML import (
     save_user,
+    save_user_login,  # NEW: Added for login tracking
     save_book,
     create_order,
     add_order_item,
@@ -18,12 +19,16 @@ from .DQL import (
     get_user_orders,
     get_user_cart,
     get_cart_total,
+    get_user_stats,        # NEW: Added for login tracking
+    get_all_users_stats,   # NEW: Added for login tracking
+    get_daily_starts,      # NEW: Added for login tracking
 )
 
 __all__ = [
     "get_db_connection",
     "create_tables",
     "save_user",
+    "save_user_login",     # NEW: Added for login tracking
     "save_book",
     "create_order",
     "add_order_item",
@@ -38,4 +43,7 @@ __all__ = [
     "get_user_orders",
     "get_user_cart",
     "get_cart_total",
+    "get_user_stats",      # NEW: Added for login tracking
+    "get_all_users_stats", # NEW: Added for login tracking
+    "get_daily_starts",    # NEW: Added for login tracking
 ]
